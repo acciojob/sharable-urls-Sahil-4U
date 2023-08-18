7710012c660e57e3eefe999e87842fb40e8d4a91
Sahil Sharma <108986297+Sahil-4U@users.noreply.github.com>
2023-08-18 23:37:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 // your code here
+const submitButtonEl=document.getElementById('button');
+const hEl=document.getElementById('url');
+submitButtonEl.addEventlistner('click',()=>{
+	hEl.innerText=`https://localhost:8080/`
+})
